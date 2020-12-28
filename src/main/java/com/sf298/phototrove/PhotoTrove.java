51ab57564e2119fbb204dbff51d3923e2b4c2ccd
sf298 <saudfatayerji@gmail.com>
2020-12-29 02:43:22 +0300

    Created the project structure

---
+++ src/main/java/com/sf298/phototrove/PhotoTrove.java
@@ -0,0 +1,13 @@
+package com.sf298.phototrove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoTrove {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoTrove.class, args);
+    }
+
+}
